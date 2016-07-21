2f57ce6bad9d8fc3284a251a4def90152e262469
Peejay Gonzales <gpeejay20@gmail.com>
2016-07-21 11:38:56 +0800

    Adding modules

---
+++ modules/commerce/modules/product/src/Entity/ProductAttributeInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Drupal\commerce_product\Entity;
+
+use Drupal\Core\Config\Entity\ConfigEntityInterface;
+
+/**
+ * Defines the interface for product attributes.
+ */
+interface ProductAttributeInterface extends ConfigEntityInterface {
+
+  /**
+   * Gets the attribute values.
+   *
+   * @return \Drupal\commerce_product\Entity\ProductAttributeValueInterface[]
+   */
+  public function getValues();
+
+  /**
+   * Gets the attribute element type.
+   *
+   * @return string
+   *   The element type name.
+   */
+  public function getElementType();
+
+}
