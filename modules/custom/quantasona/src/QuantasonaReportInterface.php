d81089b5864c72864bae469f205228ddbcd84afd
Chris McIntosh <cmcintosh@wembassy.com>
2016-08-02 09:33:12 +0800

    removing unneeded custom module.

--- modules/custom/quantasona/src/QuantasonaReportInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Drupal\quantasona;
-
-use Drupal\Core\Entity\ContentEntityInterface;
-use Drupal\user\EntityOwnerInterface;
-use Drupal\Core'Entity\EntityChangedInterface;
-
-interface QuantasonaReportInterface extends ContentEntityInterface, EntityOwnerInterface, EntityChangedInterface { }
