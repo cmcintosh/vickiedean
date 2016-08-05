44dde2d9bf20a5da08adbf544fda662162a0d4f9
Mitzi Hazel Tizon <mitzihazel_15@yahoo.com>
2016-08-05 14:07:53 +0800

    Started creating the report file upload entity.

---
+++ modules/custom/vickie/src/Entity/Controller/VickieReportFileUploadListBuilder.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Drupal\vickie\Entity\Controller;
+
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Entity\EntityListBuilder;
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Routing\UrlGeneratorInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a list controller for VickieReportFileUpload entity.
+ *
+ * @ingroup vickie
+ */
+class VickieReportFileUploadListBuilder extends EntityListBuilder {
+
+  /**
+   * The url generator.
+   *
+   * @var \Drupal\Core\Routing\UrlGeneratorInterface
+   */
+  protected $urlGenerator;
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function createInstance(ContainerInterface $container, EntityTypeInterface $entity_type) {
+    return new static(
+      $entity_type,
+      $container->get('entity.manager')->getStorage($entity_type->id()),
+      $container->get('url_generator')
+    );
+  }
+
+  /**
+   * Constructs a new VickieReportFileUploadListBuilder object.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeInterface $entity_type
+   *   The entity type definition.
+   * @param \Drupal\Core\Entity\EntityStorageInterface $storage
+   *   The entity storage class.
+   * @param \Drupal\Core\Routing\UrlGeneratorInterface $url_generator
+   *   The url generator.
+   */
+  public function __construct(EntityTypeInterface $entity_type, EntityStorageInterface $storage, UrlGeneratorInterface $url_generator) {
+    parent::__construct($entity_type, $storage);
+    $this->urlGenerator = $url_generator;
+  }
+
+
+  /**
+   * {@inheritdoc}
+   *
+   * We override ::render() so that we can add our own content above the table.
+   * parent::render() is where EntityListBuilder creates the table using our
+   * buildHeader() and buildRow() implementations.
+   */
+  public function render() {
+    $build['description'] = array(
+      '#markup' => $this->t('This is the entity for the file uploads in the Vickie Report. You can manage the fields on the <a href="@adminlink">Vickie Report admin page</a>.', array(
+        '@adminlink' => $this->urlGenerator->generateFromRoute('vickie.vickie_report_file_upload_settings'),
+      )),
+    );
+    $build['table'] = parent::render();
+    return $build;
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * Building the header and content lines for the report list.
+   *
+   * Calling the parent::buildHeader() adds a column for the possible actions
+   * and inserts the 'edit' and 'delete' links as defined for the entity type.
+   */
+  public function buildHeader() {
+    $header['id'] = $this->t('ReportID');
+    $header['name'] = $this->t('Name');
+    $header['bad_mic'] = $this->t('Bad Mic');
+    $header['location'] = $this->t('Location');
+    $header['phone'] = $this->t('Phone');
+    $header['audio'] = $this->t('Audio');
+    $header['csv'] = $this->t('CSV');
+    return $header + parent::buildHeader();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildRow(EntityInterface $entity) {
+    /* @var $entity \Drupal\vickie\Entity\Report */
+    $row['id'] = $entity->id();
+    $row['name'] = $entity->link();
+    $row['bad_mic'] = $entity->bad_mic->value;
+    $row['location'] = $entity->location->value;
+    $row['phone'] = $entity->phone->value;
+    $row['audio'] = $entity->audio->value;
+    $row['csv'] = $entity->csv->value;
+    return $row + parent::buildRow($entity);
+  }
+
+}
---
+++ modules/custom/vickie/src/Entity/VickieReportFileUpload.php
@@ -0,0 +1,333 @@
+<?php
+
+namespace Drupal\vickie\Entity;
+
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Field\BaseFieldDefinition;
+use Drupal\Core\Entity\ContentEntityBase;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\vickie\VickieReportFileUploadInterface;
+use Drupal\user\UserInterface;
+use Drupal\Core\Entity\EntityChangedTrait;
+use Drupal\Core\Field\FieldStorageDefinitionInterface;
+
+/**
+ * Defines the VickieReportFileUpload entity.
+ *
+ * @ingroup vickie
+ *
+ * @ContentEntityType(
+ *   id = "vickie_report_file_upload",
+ *   label = @Translation("Report File Uploads"),
+ *   handlers = {
+ *     "view_builder" = "Drupal\Core\Entity\EntityViewBuilder",
+ *     "list_builder" = "Drupal\vickie\Entity\Controller\VickieReportListBuilder",
+ *     "form" = {
+ *       "add" = "Drupal\vickie\Form\VickieReportForm",
+ *       "edit" = "Drupal\vickie\Form\VickieReportForm",
+ *       "delete" = "Drupal\vickie\Form\VickieReportDeleteForm",
+ *     },
+ *     "access" = "Drupal\vickie\VickieReportAccessControlHandler",
+ *   },
+ *   list_cache_contexts = { "user" },
+ *   base_table = "vickie_report_file_upload",
+ *   admin_permission = "administer report entity",
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "label" = "name",
+ *     "uuid" = "uuid"
+ *   },
+ *   links = {
+ *     "canonical" = "/report/{vickie_report_file_upload}",
+ *     "edit-form" = "/report/{vickie_report_file_upload}/edit",
+ *     "delete-form" = "/report/{vickie_report_file_upload}/delete",
+ *     "collection" = "/admin/content/vickie_report_file_upload/list"
+ *   },
+ *   field_ui_base_route = "vickie.vickie_report_file_upload_settings",
+ * )
+ *
+ */
+
+ class VickieReportFileUpload extends ContentEntityBase implements VickieReportInterface {
+
+   use EntityChangedTrait;
+
+   /**
+   * {@inheritdoc}
+   */
+   public static function preCreate(EntityStorageInterface $storage_controller, array &$values){
+     parent::preCreate($storage_controller, $values);
+     $values += array(
+       'user_id' => \Drupal::currentUser()->id(),
+     );
+   }
+
+   /**
+   * {@inheritdoc}
+   */
+   public function getID() {
+     return $this->get('id')->value;
+   }
+
+   /**
+   * {@inheritdoc}
+   */
+    public function getLocation() {
+      return $this->get('location')->value;
+    }
+
+   /**
+   * {@inheritdoc}
+   */
+    public function setLocation($location) {
+      $this->get('location')->value = $location;
+      return $this;
+    }
+
+   /**
+   * {@inheritdoc}
+   */
+    public function getPhone() {
+      return $this->get('phone')->value;
+    }
+
+   /**
+   * {@inheritdoc}
+   */
+    public function setPhone($phone) {
+      $this->get('phone')->value = $phone;
+      return $this;
+    }
+
+   /**
+   * {@inheritdoc}
+   */
+   public function getCreatedTime() {
+     return $this->get('created')->value;
+   }
+
+   /**
+   * {@inheritdoc}
+   */
+   public function getChangedTime() {
+     return $this->get('changed')->value;
+   }
+
+   /**
+   * {@inheritdoc}
+   */
+   public function getOwner() {
+     return $this->get('user_id')->entity;
+   }
+
+   /**
+   * {@inheritdoc}
+   */
+   public function getOwnerId() {
+     return $this->get('user_id')->target_id;
+   }
+
+   /**
+   * {@inheritdoc}
+   */
+   public function setOwnerId($uid) {
+     $this->set('user_id', $uid);
+     return $this;
+   }
+
+   /**
+   * {@inheritdoc}
+   */
+   public function setOwner(UserInterface $account) {
+     $this->set('user_id', $account->id());
+     return $this;
+   }
+
+   /**
+   * {@inheritdoc}
+   */
+   public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
+
+     // Standard fields, used for a primary index
+
+      $fields['id'] = BaseFieldDefinition::create('integer')
+       ->setLabel(t('ID'))
+       ->setDescription(t('The Unique ID for this report.'))
+       ->setReadOnly(TRUE);
+
+      $fields['uuid'] = BaseFieldDefinition::create('uuid')
+        ->setLabel(t('UUID'))
+        ->setDescription(t('The UUID of the Contact entity.'))
+        ->setReadOnly(TRUE);
+
+      $fields['name'] = BaseFieldDefinition::create('string')
+        ->setLabel(t('Report Name'))
+        ->setDescription(t('The name of the Report entity.'))
+        ->setSettings(array(
+          'default_value' => '',
+          'max_length' => 255,
+          'text_processing' => 0,
+        ))
+        ->setDisplayOptions('view', array(
+          'label' => 'above',
+          'type' => 'string',
+          'weight' => -6
+        ))
+        ->setDisplayOptions('form', array(
+          'type' => 'string_textfield',
+          'weight' => -6
+        ))
+        ->setDisplayConfigurable('form', TRUE)
+        ->setdisplayConfigurable('view', TRUE);
+
+      $fields['bad_mic'] = BaseFieldDefinition::create('boolean')
+        ->setLabel(t('Bad Mic'))
+        ->setDescription(t('Stores the status of a bad mic.'))
+        ->setSettings(array( 'default_value' => 0))
+        ->setDisplayOptions('view', array(
+          'label' => 'above',
+          'type' => 'string',
+          'weight' => -5
+        ))
+        ->setdisplayOptions('form', array(
+          'type' => 'boolean_checkbox',
+          'weight' => -5
+        ))
+        ->setDisplayConfigurable('form', TRUE)
+        ->setdisplayConfigurable('view', TRUE);
+
+      $fields['location'] = BaseFieldDefinition::create('string')
+        ->setLabel(t('Location'))
+        ->setSettings(array(
+          'default_value' => '',
+          'max_length' => 255,
+          'text_processing' => 0,
+        ))
+        ->setDisplayOptions('view', array(
+          'label' => 'above',
+          'type' => 'string',
+          'weight' => -4
+        ))
+        ->setDisplayOptions('form', array(
+          'type' => 'string_textfield',
+          'weight' => -4
+        ))
+        ->setDisplayConfigurable('form', TRUE)
+        ->setdisplayConfigurable('view', TRUE);
+
+      $fields['gender'] = BaseFieldDefinition::create('string')
+        ->setLabel(t('Gender'))
+        ->setSettings(array(
+          'default_value' => '',
+          'max_length' => 255,
+          'text_processing' => 0,
+        ))
+        ->setDisplayOptions('view', array(
+          'label' => 'above',
+          'type' => 'string',
+          'weight' => -4
+        ))
+        ->setDisplayOptions('form', array(
+          'type' => 'string_textfield',
+          'weight' => -4
+        ))
+        ->setDisplayConfigurable('form', TRUE)
+        ->setdisplayConfigurable('view', TRUE);
+
+      $fields['age'] = BaseFieldDefinition::create('string')
+        ->setLabel(t('Age'))
+        ->setSettings(array(
+          'default_value' => '',
+          'max_length' => 255,
+          'text_processing' => 0,
+        ))
+        ->setDisplayOptions('view', array(
+          'label' => 'above',
+          'type' => 'string',
+          'weight' => -4
+        ))
+        ->setDisplayOptions('form', array(
+          'type' => 'string_textfield',
+          'weight' => -4
+        ))
+        ->setDisplayConfigurable('form', TRUE)
+        ->setdisplayConfigurable('view', TRUE);
+
+      $fields['phone'] = BaseFieldDefinition::create('string')
+          ->setLabel(t('Phone'))
+          ->setSettings(array(
+            'default_value' => '',
+            'max_length' => 255,
+            'text_processing' => 0,
+          ))
+          ->setDisplayOptions('view', array(
+            'label' => 'above',
+            'type' => 'string',
+            'weight' => -4
+          ))
+          ->setDisplayOptions('form', array(
+            'type' => 'string_textfield',
+            'weight' => -4
+          ))
+          ->setDisplayConfigurable('form', TRUE)
+          ->setdisplayConfigurable('view', TRUE);
+
+      $fields['email'] = BaseFieldDefinition::create('string')
+        ->setLabel(t('Email'))
+        ->setSettings(array(
+          'default_value' => '',
+          'max_length' => 255,
+          'text_processing' => 0,
+        ))
+        ->setDisplayOptions('view', array(
+          'label' => 'above',
+          'type' => 'string',
+          'weight' => -4
+        ))
+        ->setDisplayOptions('form', array(
+          'type' => 'string_textfield',
+          'weight' => -4
+        ))
+        ->setDisplayConfigurable('form', TRUE)
+        ->setdisplayConfigurable('view', TRUE);
+
+      $fields['audio'] = BaseFieldDefinition::create('file')
+          ->setLabel(t('Audio Files'))
+          ->setDescription(t('Report audio files uploaded for this report.'))
+          ->setSetting('file_extensions', 'mp3 wav')
+          ->setDefaultValue('')
+          ->setDisplayOptions('view', array(
+            'label' => 'above',
+            'type' => 'file',
+            'weight' => -4,
+          ))
+          ->setDisplayOptions('form', array(
+            'type' => 'file',
+            'weight' => -4,
+          ))
+          ->setDisplayConfigurable('form', TRUE)
+          ->setDisplayConfigurable('view', TRUE)
+          ->setCardinality(FieldStorageDefinitionInterface::CARDINALITY_UNLIMITED);
+
+      $fields['csv'] = BaseFieldDefinition::create('file')
+        ->setLabel(t('CSV File'))
+        ->setDescription(t('Report CSV files uploaded for this report.'))
+        ->setSetting('file_extensions', 'csv')
+        ->setDefaultValue('')
+        ->setDisplayOptions('view', array(
+          'label' => 'above',
+          'type' => 'file',
+          'weight' => -4,
+        ))
+        ->setDisplayOptions('form', array(
+          'type' => 'file',
+          'weight' => -4,
+        ))
+        ->setDisplayConfigurable('form', TRUE)
+        ->setDisplayConfigurable('view', TRUE)
+        ->setCardinality(FieldStorageDefinitionInterface::CARDINALITY_UNLIMITED);
+
+      return $fields;
+   }
+
+ }
